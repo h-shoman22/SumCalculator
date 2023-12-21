a537651b33614d8b150b1f0812320810a1851f82
h-shoman22 <153935339+h-shoman22@users.noreply.github.com>
2023-12-21 18:14:22 +0000

    Add files via upload

---
+++ SumCalculator.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class SumCalculator {
+    public int firstNumber;
+    public int secondNumber;
+    
+
+    public SumCalculator(int sFirstNumber, int sSecondNumber){
+        firstNumber = sFirstNumber;
+        secondNumber = sSecondNumber;
+    }
+
+    public int calculateSum(){
+        int answer =  firstNumber + secondNumber;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        // SumCalculator total = new SumCalculator(8, 15);
+        // System.out.println(total.calculateSum());
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter first number: ");
+        int number1 = scanner.nextInt();
+        System.out.println("enter the second number: ");
+        int number2 = scanner.nextInt();
+        SumCalculator total = new SumCalculator(number1, number2);
+        System.out.println(total.calculateSum());    
+    }
+
+    /*Create an instance of your calculator class
+
+    Print me a line which says ‘ The sum of both numbers is: ANSWER */
+    
+}
